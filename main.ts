acc35ae3ac49874a2b20df9a6b66e0022ce615b9
kakaustar <165193372+kakaustar@users.noreply.github.com>
2024-04-12 11:51:18 -0300

    Atualizar 'pxt.json, main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    definir_contador = 10
+    for (let index = 0; index < 10; index++) {
+        tempo += -1
+        basic.showNumber(tempo)
+        basic.pause(100)
+    }
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . # . #
+        # . . . #
+        . # # # .
+        `)
+    music.play(music.stringPlayable("C C E D F E A D ", 120), music.PlaybackMode.UntilDone)
+})
+let tempo = 0
+let definir_contador = 0
+basic.showNumber(0)
 let contador = 10
